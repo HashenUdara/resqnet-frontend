79125c664c95a04a85b27368aa81e85fbc5a4115
hashen <emhashenudara@gmail.com>
2025-10-14 08:20:37 +0530

    Add signup pages and initial database schema
    Introduces HTML signup forms for general users, NGOs, and volunteers, each with client-side validation and tailored fields. Adds an initial SQL schema for users, volunteers, general users, NGOs, skills, preferences, and Grama Niladhari, supporting role-based registration and data structure.

---
+++ sql.sql
@@ -0,0 +1,73 @@
+users(
+  user_id INT PK,
+  username VARCHAR(100),
+  password VARCHAR(255),
+  email VARCHAR(150),
+  role ENUM('general','volunteer','ngo','grama_niladhari','dmc')
+);
+
+volunteers(
+  user_id INT PK FK → users(user_id),
+  name VARCHAR(150),
+  age INT,
+  gender ENUM('male','female','other'),
+  contact_number VARCHAR(20),
+  hourse_no VARCHAR(50),
+  street VARCHAR(100),
+  city VARCHAR(100),
+  district VARCHAR(100),
+  gn_division VARCHAR(100)
+);
+ general_user(
+  user_id INT PK FK → users(user_id),
+  name VARCHAR(150),
+  contact_number VARCHAR(20),
+  hourse_no VARCHAR(50),
+  street VARCHAR(100),
+  city VARCHAR(100),
+  district VARCHAR(100),
+  gn_division VARCHAR(100)
+  sms_alert BOOLEAN DEFAULT FALSE
+ )
+
+skills(
+  skill_id INT PK,
+  skill_name VARCHAR(100)
+)
+
+skills_volunteers(
+  user_id INT PK FK → volunteers(user_id),
+  skill_id INT PK FK → skills(skill_id)
+);
+
+volunteer_preferences(
+  preference_id INT PK,
+  preference_name VARCHAR(100)
+);
+
+volunteer_preference_volunteers(
+  user_id INT PK FK → volunteers(user_id),
+  preference_id INT PK FK → volunteer_preferences(preference_id)
+);
+
+ngos(
+  user_id INT PK FK → users(user_id),
+  organization_name VARCHAR(150),
+    registration_number VARCHAR(100),
+    years_of_operation INT,
+    address VARCHAR(255),
+  contact_person_name VARCHAR(100),
+  contact_person_telephone VARCHAR(20),
+  contact_person_email VARCHAR(150)
+
+);
+
+grama_niladhari(
+    user_id INT PK FK → users(user_id),
+    name VARCHAR(150),
+    contact_number VARCHAR(20),
+    address VARCHAR(255),
+    gn_division VARCHAR(100),
+    service_number VARCHAR(50),
+    gn_division_number VARCHAR(50),
+)
